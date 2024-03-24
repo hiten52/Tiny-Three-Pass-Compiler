5844f1ad78c7fa0b6b45c956e4400e40d370b79d
hiten52 <hitensolanki010@gmail.com>
2024-03-24 19:06:02 +0530

    Initial project structure

---
+++ app/src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+}
