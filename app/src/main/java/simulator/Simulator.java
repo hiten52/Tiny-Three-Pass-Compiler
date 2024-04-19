2fd9e5dd35fc363d69132e14aaf39adef101fc72
hiten52 <hitensolanki010@gmail.com>
2024-04-19 13:01:03 +0530

    Add simulate function

--- app/src/main/java/simulator/Simulator.java
+++ app/src/main/java/simulator/Simulator.java
@@ -7,8 +7,6 @@ import java.util.Stack;
  * Simple stack machine with two registers.
  */
 public class Simulator {
-  ArrayList<String> asm; // Assembly instructions
-  ArrayList<Integer> args; // Arguments for instructions
   Integer r0; // Register 0
   Integer r1; // Register 1
   Integer ip; // Instruction Pointer
@@ -16,23 +14,68 @@ public class Simulator {
 
   /*
    * Create a machine with pre-defined assembly code and arguments.
-   * @param asm ArrayList with assembly code
-   * @param args ArrayList of arguments as integer
    */
-  public Simulator(ArrayList<String> asm, ArrayList<Integer> args) {
-    this.asm = asm;
-    this.args = args;
+  public Simulator() {
     this.r0 = null;
     this.r1 = null;
     this.ip = 0;
     stack = new Stack<Integer>();
+
+  }
+
+  /*
+   * Simulates the machine based on the provided assembly instructions.
+   * 
+   * @param asm ArrayList with assembly code
+   * @param args ArrayList of arguments as integer
+   */
+  public void simulate(ArrayList<String> asm, ArrayList<Integer> args) {
+    while (ip < asm.size()) {
+      String instruct = asm.get(ip);
+      String[] parts = instruct.split("\\s+");
+      String ins = parts[0];
+      Integer n = parts.length > 1 ? Integer.parseInt(parts[1]) : null;
+
+      switch (ins) {
+        case "IM":
+          r0 = n;
+          break;
+        case "AR":
+          r0 = args.get(n);
+          break;
+        case "SW":
+          int tmp = r0;
+          r0 = r1;
+          r1 = tmp;
+          break;
+        case "PU":
+          stack.push(r0);
+          break;
+        case "PO":
+          r0 = stack.pop();
+          break;
+        case "AD":
+          r0 += r1;
+          break;
+        case "SU":
+          r0 -= r1;
+          break;
+        case "MU":
+          r0 *= r1;
+          break;
+        case "DI":
+          r0 /= r1;
+          break;
+      }
+      ip++;
+    }
   }
 
   /**
    * Returns a string representation of current machine state.
    *
    * @return A string represenatation of current machine state, including registers values,
-   *     intruction pointer andd stack.
+   *         intruction pointer andd stack.
    */
   public String toString() {
     String r0RegStr = String.format("%d\n", this.r0);
